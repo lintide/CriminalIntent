12efa4c3e4bd0600f7529a4f5a9fdc19d560a7ed
lintide <lintide@gmail.com>
2013-10-27 23:36:43 +0800

    ch07

--- src/com/example/criminalintent/CrimeActivity.java
+++ src/com/example/criminalintent/CrimeActivity.java
@@ -1,7 +1,10 @@
 package com.example.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,6 +12,17 @@ public class CrimeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if (fragment == null) {
+			Log.d("Crim", "create crime fragment");
+			fragment = new CrimeFragment();
+			fm.beginTransaction()
+				.add(R.id.fragmentContainer, fragment)
+				.commit();
+		}
 	}
 
 }
--- src/com/example/criminalintent/CrimeFragment.java
+++ src/com/example/criminalintent/CrimeFragment.java
@@ -2,13 +2,18 @@ package com.example.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
 	private Crime mCrime;
+	private EditText mTtileField;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,31 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceBundle) {
 		View view = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		mTtileField = (EditText)view.findViewById(R.id.crime_title);
+		mTtileField.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+//				Log.d("Crime", "edit text changed");
+				mCrime.setTitle(s.toString());
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+//				Log.d("Crime", "edit before text changed");
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+//				Log.d("Crime", "edit after text changed");
+				
+			}
+		});
 		
 		return view;
 	}
